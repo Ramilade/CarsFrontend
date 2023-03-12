37ab875e3739f3af62a0423a0f9120a6ae1f97b0
Ramilade <rami0904@stud.kea.dk>
2023-03-12 20:22:25 +0100

    API FIX

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://rami0904.azurewebsites.net/api"
+export const API_URL = "https://rami0904.azurewebsites.net/api"
 //export const API_URL = "http://localhost:8080/api"
 
 
