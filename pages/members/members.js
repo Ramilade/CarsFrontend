351e1b9bbe0d7a5942e8e7808404814012eaf6da
Ramilade <rami0904@stud.kea.dk>
2023-03-12 10:45:44 +0100

    Members fixed

--- pages/members/members.js
+++ pages/members/members.js
@@ -1,6 +1,27 @@
-import { API_URL } from "../../settings.js"
+//Skal være admin når roles kommer på
+
+import { API_URL } from "../../settings.js";
+import { handleHttpErrors, sanitizeStringWithTableRows } from "../../utils.js";
 const URL = API_URL + "/members"
 
-export function initMembers(){
 
+
+function showAllMembers(data) {
+  const tableRowsArray = data.map((member) => `<tr>
+  <td>${member.username}</td><td>${member.email}</td><td>${member.firstName} ${member.lastName}</td>
+</tr>`)
+
+// <td>${member.ranking}</td>
+
+const tableRowString = tableRowsArray.join("\n");
+document.getElementById("tbl-body").innerHTML = sanitizeStringWithTableRows(tableRowString);
+}
+
+export async function initMembers(){
+  try {
+    const data = await fetch(URL).then((res) => res.json());
+    showAllMembers(data);
+  } catch (err) {
+    handleHttpErrors(err);
+  }
 }
