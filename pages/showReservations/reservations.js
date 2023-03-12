c78c44f1e432445dfac96b8b69ceb456ac209027
Ramilade <rami0904@stud.kea.dk>
2023-03-12 12:51:00 +0100

    Reservations added(working=)

--- pages/showReservations/reservations.js
+++ pages/showReservations/reservations.js
@@ -1,5 +1,25 @@
-import { API_URL} from "../../settings.js"
+import { API_URL } from "../../settings.js";
 
 export async function initListReservationsAll() {
-}
+  const tableBody = document.querySelector("#tablerows");
+  const errorParagraph = document.querySelector("#error");
+  
+  try {
+    const response = await fetch(API_URL + "/reservations");
+    const reservations = await response.json();
 
+    reservations.forEach((reservation) => {
+      const tableRow = document.createElement("tr");
+      tableRow.innerHTML = `
+      <td>${reservation.carId}</td>
+      <td>${reservation.carBrand}</td>
+      <td>${reservation.carModel}</td>
+      <td>${reservation.reservationDate}</td>
+      <td>${reservation.price}</td>
+      `;
+      tableBody.appendChild(tableRow);
+    });
+  } catch (error) {
+    errorParagraph.textContent = "An error occurred while fetching the data.";
+  }
+}
